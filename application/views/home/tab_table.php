e12c486925ab8144008fed5925075cb8ff571ea4
salungp <salungprastyo@gmail.com>
2019-11-16 09:26:03 +0700

    Fixing datatable error at page index

--- application/views/home/tab_table.php
+++ application/views/home/tab_table.php
@@ -1,16 +1,19 @@
 <div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
 	<a href="" class="btn btn-success mb-3"><i class="fa fa-file-excel-o"></i> Ekspor excel</a>
 	<?php echo $this->session->flashdata('message'); ?>
-	<table class="table table-bordered" id="table-one">
+	<table class="table table-bordered">
 	  <thead>
 	    <tr>
 	      <th scope="col">No</th>
-	      <th scope="col">Action</th>
+	      <?php if ($this->session->has_userdata('user_id')) : ?>
+		      <th scope="col">Action</th>
+		    <?php endif; ?>
 	      <th scope="col">Detail</th>
 	      <th scope="col">Jenis kegiatan</th>
 	      <th scope="col">Jumlah biaya</th>
 	    </tr>
 	  </thead>
+
 	  <tbody>
 	  	<?php $i = 1; ?>
 	  	<?php foreach($jenis_kegiatan as $item) : ?>
@@ -26,20 +29,23 @@
 	  			}
 	  		?>
 		    <tr>
-		      <th scope="row"><?php echo $i++; ?></th>
-		      <td>
-		      	<a data-toggle="tooltip" title="Edit" class="btn btn-primary mb-1" href="<?php echo base_url('edit_jenis_kegiatan/'.$item['id']); ?>"><i class="fa fa-edit"></i></a>
-		      	<a data-toggle="tooltip" title="Hapus" class="btn btn-danger mb-1" href="<?php echo base_url('delete_jenis_kegiatan/'.$item['id']); ?>" onclick="return window.confirm('Yakin mau dihapus?');"><i class="fa fa-trash"></i></a>
-		      </td>
+		      <td scope="row"><?php echo $i++; ?></td>
+		      <?php if ($this->session->has_userdata('user_id')) : ?>
+			      <td>
+			      	<a data-toggle="tooltip" title="Edit" class="btn btn-primary mb-1" href="<?php echo base_url('edit_jenis_kegiatan/'.$item['id']); ?>"><i class="fa fa-edit"></i></a>
+			      	<a data-toggle="tooltip" title="Hapus" class="btn btn-danger mb-1" href="<?php echo base_url('delete_jenis_kegiatan/'.$item['id']); ?>" onclick="return window.confirm('Yakin mau dihapus?');"><i class="fa fa-trash"></i></a>
+			      </td>
+			    <?php endif; ?>
 		      <td><a href="<?php echo base_url('detail_jenis_kegiatan/'.$item['id']); ?>"><i class="fa fa-external-link"></i> Detail</a></td>
 		      <td><h5><?php echo $item['title']; ?></h5></td>
 		      <td><strong>Rp <?php echo number_format($jumlah, 2, ',', '.'); ?></strong></td>
 		    </tr>
 	  	<?php endforeach; ?>
 	  	<tr>
-	  		<td colspan="4" class="text-center"><h5>Jumlah total</h5></td>
+	  		<td colspan="<?php if($this->session->has_userdata('user_id')) { echo '5'; } else { echo '3'; } ?>" class="text-center"><h5>Jumlah total</h5></td>
 	  		<td><strong>Rp <?php echo number_format($semua, 2, ',', '.'); ?></strong></td>
 	  	</tr>
 	  </tbody>
+
 	</table>
 </div>
