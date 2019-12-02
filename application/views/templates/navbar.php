34c429c6d995e4ee7c270f0642a08de92cdc71af
salungp <salungprastyo@gmail.com>
2019-12-02 08:09:22 +0700

    maintenance navbar

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -5,27 +5,52 @@
 	    <a href="<?php echo base_url(); ?>" class="brand-logo">Logo</a>
 	    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fa fa-bars"></i></a>
 	    <ul class="right hide-on-med-and-down">
-	      <li class="waves-effect waves-light"><a href="<?php echo base_url('home'); ?>"><i class="fa fa-home"></i> Beranda</a></li>
-	      <?php if ($this->session->has_userdata('user_id')) : ?>
-	      	<li class="waves-effect waves-light"><a href="<?php echo base_url('list/create'); ?>"><i class="fa fa-pencil"></i> Create list kegiatan</a></li>
-	      	<li class="waves-effect waves-light"><a href="<?php echo base_url('logout'); ?>"><i class="fa fa-sign-out"></i> Logout</a></li>
-				<?php else : ?>
-					<li class="waves-effect waves-light"><a href="<?php echo base_url('login'); ?>"><i class="fa fa-sign-in"></i> Login</a></li>	      	
-	      <?php endif; ?>
+	      <li class="waves-effect waves-light">
+	      	<a href="<?php echo base_url('home'); ?>">
+	      		<i class="fa fa-home"></i> Beranda
+	      	</a>
+	      </li>
+
+      	<li class="waves-effect waves-light">
+      		<a href="<?php echo base_url('list/create'); ?>">
+      			<i class="fa fa-pencil"></i> Create list kegiatan
+      		</a>
+      	</li>
+
+      	<li class="waves-effect waves-light">
+      		<a href="<?php echo base_url('logout'); ?>">
+      			<i class="fa fa-sign-out"></i> Logout
+      		</a>
+      	</li>
 	    </ul>
 	  </div>
 	</div>
 </nav>
 
 <ul class="sidenav" id="mobile-demo">
-  <li class="waves-effect waves-light"><a href="<?php echo base_url(); ?>"><i class="fa fa-home"></i> Beranda</a></li>
-  <?php if ($this->session->has_userdata('user_id')) : ?>
-  	<li class="waves-effect waves-light"><a href="<?php echo base_url('create_list_kegiatan'); ?>"><i class="fa fa-pencil"></i> Create list kegiatan</a></li>
-	  <li class="waves-effect waves-light"><a href="<?php echo base_url('create_detail_kegiatan'); ?>"><i class="fa fa-pencil"></i> Judul kegiatan</a></li>
-  	<li class="waves-effect waves-light"><a href="<?php echo base_url('logout'); ?>"><i class="fa fa-sign-out"></i> Logout</a></li>
-  <?php else : ?>
-		<li class="waves-effect waves-light"><a href="<?php echo base_url('login'); ?>"><i class="fa fa-sign-in"></i> Login</a></li>	 
-  <?php endif; ?>
+  <li class="waves-effect waves-light">
+  	<a href="<?php echo base_url(); ?>">\
+  		<i class="fa fa-home"></i> Beranda
+  	</a>
+  </li>
+
+	<li class="waves-effect waves-light">
+		<a href="<?php echo base_url('create_list_kegiatan'); ?>">
+			<i class="fa fa-pencil"></i> Create list kegiatan
+		</a>
+	</li>
+
+  <li class="waves-effect waves-light">
+  	<a href="<?php echo base_url('create_detail_kegiatan'); ?>">
+  		<i class="fa fa-pencil"></i> Judul kegiatan
+  	</a>
+  </li>
+  
+	<li class="waves-effect waves-light">
+		<a href="<?php echo base_url('logout'); ?>">
+			<i class="fa fa-sign-out"></i> Logout
+		</a>
+	</li>
 </ul>
 
 <div class="container">
