84551dc0167e804508d49ed2efd545b7f7c0414f
salungp <salungprastyo@gmail.com>
2019-12-03 18:53:26 +0700

    Maintenance page home/index

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -29,7 +29,7 @@
             <div class="inner">
               <h3>0</h3>
 
-              <p>Bounce Rate</p>
+              <p>Pemasukan</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
@@ -43,24 +43,10 @@
             <div class="inner">
               <h3>0</h3>
 
-              <p>User Registrations</p>
+              <p>Penegeluaran</p>
             </div>
             <div class="icon">
               <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-xs-6">
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>0</h3>
-
-              <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
             </div>
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
