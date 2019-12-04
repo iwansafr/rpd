bf37c2cd920cf4bc51425a38c2c401a1f87079fb
salungp <salungprastyo@gmail.com>
2019-12-04 10:16:04 +0700

    Fixing login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,98 +1,72 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php $this->load->view('templates/meta', ['title' => 'Login']); ?>
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Keuangan | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/'); ?>iCheck/square/blue.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Admin</b>LTE</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Sign in to start your session</p>
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-      html,
-			body {
-			  height: 100%;
-			}
+    <form action="<?php echo base_url('auth/login'); ?>" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" name="username" class="form-control" placeholder="Username">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
 
-			body {
-			  display: -ms-flexbox;
-			  display: flex;
-			  -ms-flex-align: center;
-			  align-items: center;
-			  padding-top: 40px;
-			  padding-bottom: 40px;
-			  background-color: #f5f5f5;
-			}
-
-			.form-signin {
-			  width: 100%;
-			  max-width: 330px;
-			  padding: 15px;
-			  margin: auto;
-			}
-			.form-signin .checkbox {
-			  font-weight: 400;
-			}
-			.form-signin .form-control {
-			  position: relative;
-			  box-sizing: border-box;
-			  height: auto;
-			  padding: 10px;
-			  font-size: 16px;
-			}
-			.form-signin .form-control:focus {
-			  z-index: 2;
-			}
-			.form-signin input[type="email"] {
-			  margin-bottom: -1px;
-			  border-bottom-right-radius: 0;
-			  border-bottom-left-radius: 0;
-			}
-			.form-signin input[type="password"] {
-			  margin-bottom: 10px;
-			  border-top-left-radius: 0;
-			  border-top-right-radius: 0;
-			}
-
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-  </head>
-  <body class="text-center">
-    <form class="form-signin" action="<?php echo base_url('auth/login'); ?>" method="POST">
-		  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-		  <?php echo $this->session->flashdata('message'); ?>
-
-		  <div class="input-field">
-			  <input type="text" name="username" id="inputEmail" class="form-control <?php echo form_error('username') ? 'is-invalid' : '' ?>" placeholder="Username" autofocus>
-			  <?php echo form_error('username', '<small class="invalid-feedback mb-1">', '</small>'); ?>
-			  <label for="inputEmail">Email address</label>
-		  </div>
-
-		  <div class="input-field">
-			  <input type="password" name="password" id="inputPassword" class="form-control <?php echo form_error('password') ? 'is-invalid' : '' ?>" placeholder="Password">
-			  <label for="inputPassword">Password</label>
-			  <?php echo form_error('login', '<small class="invalid-feedback mb-1">', '</small>'); ?>
-		  </div>
-
-		  <p>
-		    <label>
-		      <input type="checkbox" class="filled-in">
-		      <span>Remember me</span>
-		    </label>
-		  </p>
-
-		  <button class="btn waves-effect waves-light blue" type="submit">Sign in</button>
-		  <p class="mt-5 mb-3 text-muted">&copy; 2019-2019</p>
-		</form>
-<?php $this->load->view('templates/script'); ?>
+<!-- jQuery 3 -->
+<script src="<?php echo base_url('assets/'); ?>js/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url('assets/'); ?>js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url('assets/'); ?>iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
 </body>
 </html>
